d524a8bc42572695e9ec9c571efb59ffc146174a
Arthur Brum <arthur@example.com>
2017-11-20 04:32:03 -0200

    numpy array from microphone

--- rawConvolveTest.py
+++ rawConvolveTest.py
@@ -11,46 +11,52 @@ seventh = (15/8.0)*first
 
 list1 = 2*np.pi*np.array([first, third, fifth, seventh, seventh, fifth, first, third])
 
-n_points = 0.1              # sampling rate, Hz, must be integer
+n_points = 0.1              # 'tempo' que a frequencia eh 'tocada'
 
-n_items = list1.size        # numeros de itens (freqs no caso)
-t_end = n_items*n_points    # tamanho total no eixo X
+n_items = list1.size        # numeros de itens (freqs, no caso)
+t_end = n_items*n_points    # tamanho total no eixo X ('tempo' total)
 
 # Teorema da amostragem pede um pouco mais de 2x a maior freq
 s_rate = 2.1*list1.max()
 
-# cria vetor de tempo respeitando teorema da amostragem
+# Cria vetor de tempo
 t = np.linspace(0, t_end, s_rate*t_end)
 
-#Cria um novo vetor com mesmo tamanho de t
+#Aloca um novo vetor com mesmo tamanho de t
 samples = np.empty(t.size)
 
+# Para cada loop preenche uma parte da saida (uma frequencias por vez)
 i_begin = 0
 for i in range(1, n_items+1):
-    # Estabelece inicio e fim do vetor
+
+    # Estabelece fim da parte que sera preenchida nessa iteracao
     i_end = s_rate*n_points * i;
 
     samples[i_begin:i_end] = np.sin(list1[i-1]*t[i_begin:i_end])
 
+    # Estabelece inicio da proxima parte
     i_begin = i_end
+
 
 # adding white noise
 noise = np.random.normal(size=t.shape)
-
 samples = samples+noise
 
+
+# plot pra ver o baguiu
 plt.plot(t, samples)
-plt.xlim([0,0.1])
+#plt.xlim([0,0.1])
 plt.show()
 
-
+# Determina qual a frequencia que sera procurada criando um filtro
 filter = np.sin(list1[1]*t[0:s_rate*n_points])
 
+# Faz convolucao pelo sinal procurando 'matches' do filtro
 out= signal.fftconvolve(samples, filter, mode='full')
 
 
 # Plot the data
-plt.plot(out)
+plt.plot(t, out[0:t.size])
 plt.show()
 
 
--- receiver.py
+++ receiver.py
@@ -11,65 +11,48 @@ from scipy import signal
 
 # gravar do microfone
 
+CHUNKSIZE = 1024  # fixed chunk size
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 2
+WAVE_OUTPUT_FILENAME = "output.wav"
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+print("* recording")
+
+frames = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+print("* done recording")
+
+numpydata = np.fromstring(data, dtype=np.int16)
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+plt.plot(numpydata)
+plt.show()
+
+
 
 
 
 # passar para numpy array
 
 
-
-
-# Analise de frequencias
-
-
-first = 220.00
-third = (5/4.0)*first
-fifth = (3/2.0)*first
-seventh = (15/8.0)*first
-
-list1 = 2*np.pi*np.array([first, third, fifth, seventh, seventh, fifth, first, third])
-
-n_points = 0.1              # sampling rate, Hz, must be integer
-
-n_items = list1.size        # numeros de itens (freqs no caso)
-t_end = n_items*n_points    # tamanho total no eixo X
-
-# Teorema da amostragem pede um pouco mais de 2x a maior freq
-s_rate = 2.1*list1.max()
-
-# cria vetor de tempo respeitando teorema da amostragem
-t = np.linspace(0, t_end, s_rate*t_end)
-
-#Cria um novo vetor com mesmo tamanho de t
-samples = np.empty(t.size)
-
-i_begin = 0
-for i in range(1, n_items+1):
-    # Estabelece inicio e fim do vetor
-    i_end = s_rate*n_points * i;
-
-    samples[i_begin:i_end] = np.sin(list1[i-1]*t[i_begin:i_end])
-
-    i_begin = i_end
-
-# adding white noise
-noise = np.random.normal(size=t.shape)
-
-samples = samples+noise
-
-plt.plot(t, samples)
-plt.xlim([0,0.1])
-plt.show()
-
-filter = np.sin(list1[1]*t[0:s_rate*n_points])
-
-a= signal.fftconvolve(samples, filter, mode='full')
-
-print t_end
-print t.size
-print a.size
-# Plot the data
-plt.plot(t, a[0:t.size])
-plt.show()
 
 
